69157ba61b2b0bb781e008253b403396d232f598
ASWIN-KUMAR-5 <aswinkumar5306@gmail.com>
2024-10-18 23:46:20 +0530

    Update palindrome_checker.py

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,10 +1,8 @@
 def is_palindrome(s):
-    # Remove spaces and convert to lowercase
     cleaned_string = ''.join(s.split()).lower()
-    # Check if the cleaned string is equal to its reverse
     return cleaned_string == cleaned_string[::-1]
 
-# Example usage
+
 input_string = input("Enter a String:")
 if is_palindrome(input_string):
     print("palindrome")
